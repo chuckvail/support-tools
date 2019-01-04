466dde7d17c96bed8bf05fc5e023fa1b8e1d38ad
Chuck Vail <chuck.vail@sourceday.com>
2019-01-04 09:12:19 -0600

    Getting tools set up

--- changeset-monitor.rb
+++ changeset-monitor.rb
@@ -1,9 +1,16 @@
 #!/usr/bin/env ruby
-
+require "mysql2"
 #monitors the changesets
 
-require "mysql2"
+myquery = "select c.name, pc.company_id, po_id, po_line, po_release, pc.changeset, pc.updated_at
+from push_changesets pc
+inner join companies c on pc.company_id = c.id
+inner join purchase_orders po on pc.purchase_order_id = po.id
 
+where pc.success = 0 and pc.invalidated = 0
+order by pc.updated_at DESC;"
+
+#Connect to the database and load changesets into memory:
 connect = Mysql2::Client.new(:host => "hostname", :username => "username", :password => "password", :database => "name_db")
-result = connect.query("SELECT * FROM table_name")
+result = connect.query(myquery)
 result.each {  |x| puts x }
---
+++ iis_analyzer.ps1
@@ -0,0 +1,63 @@
+﻿#Stuff to get ready for the things:
+#==========================================
+$restartRequired = 'True'
+$isDotNetOk = 'False'
+
+
+#Check if .NET framework 4.5 is installed:
+#==========================================
+
+# PowerShell 4 - works on mine, returns "True"
+$isDotNetOk = (Get-ItemProperty "HKLM:SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full").Release -gt 378389
+
+
+
+#Enable websockets:
+#==========================================
+
+$websocketStatus = Enable-WindowsOptionalFeature -FeatureName IIS-WebSockets -Online
+$restartRequired = ($websocketStatus.RestartNeeded)
+
+
+
+
+#Check if there's an existing site using ports 80/443:
+#==========================================
+Import-Module NetSecurity
+
+
+
+
+
+#Create a firewall rule for the custom ports:
+#============================================
+new-netfirewallrule -Action Allow -Direction Inbound -Enabled True -Protocol TCP  -LocalPort 8080 -DisplayName "SourceDay IIS port inbound"
+new-netfirewallrule -Action Allow -Direction Outbound -Enabled True -Protocol TCP  -LocalPort 8080 -DisplayName "SourceDay IIS port outbound"
+
+
+
+#stuff to git the files ready for the website. (downloaded from a public git repo? or maybe from an FTP site)
+#==========================================
+
+
+
+
+
+#Stuff to  add site with appropriate settings to IIS automatically
+$site = Get-IISSite -Name gp2010
+Echo @site
+
+$apps = Get-IISAppPool
+Echo "List of Application Pools:"
+foreach ($app in $apps) {
+    Echo @app
+    }
+
+
+
+
+#Stuff to test connection string to SQL Server
+
+#Script to prime and set up permission and db user
+
+#Eventually auto-launch the integration test suite.
